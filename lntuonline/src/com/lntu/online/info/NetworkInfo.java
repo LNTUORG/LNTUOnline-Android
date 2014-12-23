78a7e7ac0e13d700901a8ab337e95f74c20dc6ff
TakWolf <takwolf@foxmail.com>
2014-12-24 04:15:31 +0800

    本地测试

--- lntuonline/src/com/lntu/online/info/NetworkInfo.java
+++ lntuonline/src/com/lntu/online/info/NetworkInfo.java
@@ -2,6 +2,8 @@ package com.lntu.online.info;
 
 public class NetworkInfo {
 
-    public static final String serverUrl = "http://lntuonline.sinaapp.com/";
+    //public static final String serverUrl = "http://lntuonline.sinaapp.com/";
+
+	public static final String serverUrl = "http://192.168.1.106:8080/lntuonline/";
 
 }
