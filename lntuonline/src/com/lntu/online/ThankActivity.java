4bd690068908cd45b9e9c2540d22ba6c12115050
TakWolf <takwolf@foxmail.com>
2014-12-26 23:03:10 +0800

    上线最终稿

--- lntuonline/src/com/lntu/online/ThankActivity.java
+++ lntuonline/src/com/lntu/online/ThankActivity.java
@@ -13,12 +13,15 @@ public class ThankActivity extends Activity {
         setContentView(R.layout.activity_thank);
         TextView tvNames = (TextView) findViewById(R.id.thank_tv_names);
         String names = "" +
-                "" +
-                "" +
-                "" +
-                "" +
-                "" +
-                "";
+                "小马哥\n\n" +
+                "兔子大叔\n\n" +
+                "LZQ1993\n\n" +
+                "晓风残月\n\n" +
+                "浪子醉梦\n\n" +
+                "关正\n\n" +
+                "开源中国社区\n\n" +
+                "新浪云计算\n\n" +
+                "以及所有默默支持我们的你们\n";
         tvNames.setText(names);
     }
 
