daa50487d81dccdd1e4f6479b1f615ae6d3c5322
TakWolf <takwolf@foxmail.com>
2015-03-26 15:21:12 +0800

    课表布局调整

--- app/src/main/java/com/lntu/online/activity/CurriculumActivity.java
+++ app/src/main/java/com/lntu/online/activity/CurriculumActivity.java
@@ -18,6 +18,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.lntu.online.R;
+import com.lntu.online.adapter.CurriculumAdapter;
 import com.lntu.online.http.HttpUtil;
 import com.lntu.online.http.NormalAuthListener;
 import com.lntu.online.http.RetryAuthListener;
@@ -42,11 +43,9 @@ public class CurriculumActivity extends ActionBarActivity {
     protected Toolbar toolbar;
 
     @InjectView(R.id.curriculum_vp_root)
-    protected ViewPager vpRoot;
+    protected ViewPager viewPager;
 
-    private static final String[] weekdayNames = {
-        "周六", "周日", "周一", "周二", "周三", "周四", "周五", "周六", "周日", "周一", "周二"
-    };
+
 
     private Time time;
     private String strTime;
@@ -57,21 +56,21 @@ public class CurriculumActivity extends ActionBarActivity {
         setContentView(R.layout.activity_curriculum);
         ButterKnife.inject(this);
 
-        setSupportActionBar(toolbar);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-
         //定义时间控件
         time = new Time();
         time.setToNow();
-        strTime = time.year + "-" + (time.month + 1) + "-" + time.monthDay + " （" + weekdayNames[(time.weekDay == 0 ? 7 : time.weekDay) + 1] + "）";
-        //ActionBar显示日期
+        strTime = time.year + "-" + (time.month + 1) + "-" + time.monthDay + " （" + CurriculumAdapter.weekdayNames[time.weekDay] + "）";
+
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setTitle(strTime);
+
         //读取本地课表
         SharedPreferences sp = getSharedPreferences("curriculum_" + UserInfo.getSavedUserId(), Context.MODE_PRIVATE);
         try {
             Curriculum cc = Curriculum.dao.fromJson(DES3.decrypt(SecretKey.SP_KEY, sp.getString("json", "")));
-            vpRoot.setAdapter(new ViewPagerAdapter(this, cc));
-            vpRoot.setCurrentItem((time.weekDay == 0 ? 7 : time.weekDay) + 1);
+            viewPager.setAdapter(new CurriculumAdapter(this, cc));
+            viewPager.setCurrentItem(time.weekDay);
             Toast.makeText(this, "上次更新时间为：" + sp.getString("update_time", "未知"), Toast.LENGTH_LONG).show();
         } catch (Exception e) {
             startNetwork();
@@ -105,8 +104,8 @@ public class CurriculumActivity extends ActionBarActivity {
             public void onSuccess(int statusCode, Header[] headers, String responseString) {
                 try {
                     Curriculum cc = Curriculum.dao.fromJson(responseString);
-                    vpRoot.setAdapter(new ViewPagerAdapter(getContext(), cc));
-                    vpRoot.setCurrentItem((time.weekDay == 0 ? 7 : time.weekDay) + 1);
+                    viewPager.setAdapter(new CurriculumAdapter(getContext(), cc));
+                    viewPager.setCurrentItem(time.weekDay);
                     //保存在本地
                     Editor editer = getSharedPreferences("curriculum_" + UserInfo.getSavedUserId(), Context.MODE_PRIVATE).edit();
                     editer.putString("json", DES3.encrypt(SecretKey.SP_KEY, responseString));
@@ -133,8 +132,8 @@ public class CurriculumActivity extends ActionBarActivity {
             public void onSuccess(int statusCode, Header[] headers, String responseString) {
                 try {
                     Curriculum cc = Curriculum.dao.fromJson(responseString);
-                    vpRoot.setAdapter(new ViewPagerAdapter(getContext(), cc));
-                    vpRoot.setCurrentItem((time.weekDay == 0 ? 7 : time.weekDay) + 1);
+                    viewPager.setAdapter(new CurriculumAdapter(getContext(), cc));
+                    viewPager.setCurrentItem(time.weekDay);
                     //保存在本地
                     Editor editer = getSharedPreferences("curriculum_" + UserInfo.getSavedUserId(), Context.MODE_PRIVATE).edit();
                     editer.putString("json", DES3.encrypt(SecretKey.SP_KEY, responseString));
@@ -149,95 +148,6 @@ public class CurriculumActivity extends ActionBarActivity {
             }
 
         });
-    }
-
-    private class ViewPagerAdapter extends PagerAdapter {
-
-        private List<View> views;
-
-        public ViewPagerAdapter(Context context, Curriculum cc) {
-            LayoutInflater inflater = LayoutInflater.from(context);
-            views = new ArrayList<View>();
-            for (int n = 0; n < weekdayNames.length; n++) {
-                View view = inflater.inflate(R.layout.activity_curriculum_item, null);
-                //时间
-                TextView tvTime1 = (TextView) view.findViewById(R.id.curriculum_tv_time1);
-                TextView tvTime2 = (TextView) view.findViewById(R.id.curriculum_tv_time2);
-                TextView tvTime3 = (TextView) view.findViewById(R.id.curriculum_tv_time3);
-                TextView tvTime4 = (TextView) view.findViewById(R.id.curriculum_tv_time4);
-                TextView tvTime5 = (TextView) view.findViewById(R.id.curriculum_tv_time5);
-                tvTime1.setText("第1节\n" + cc.getTimes().get(1) + "\n第2节\n" + cc.getTimes().get(2));
-                tvTime2.setText("第3节\n" + cc.getTimes().get(3) + "\n第4节\n" + cc.getTimes().get(4));
-                tvTime3.setText("第5节\n" + cc.getTimes().get(5) + "\n第6节\n" + cc.getTimes().get(6));
-                tvTime4.setText("第7节\n" + cc.getTimes().get(7) + "\n第8节\n" + cc.getTimes().get(8));
-                tvTime5.setText("第9节\n" + cc.getTimes().get(9) + "\n第10节\n" + cc.getTimes().get(10));
-                //课程
-                TextView tvCourse1 = (TextView) view.findViewById(R.id.curriculum_tv_course1);
-                TextView tvCourse2 = (TextView) view.findViewById(R.id.curriculum_tv_course2);
-                TextView tvCourse3 = (TextView) view.findViewById(R.id.curriculum_tv_course3);
-                TextView tvCourse4 = (TextView) view.findViewById(R.id.curriculum_tv_course4);
-                TextView tvCourse5 = (TextView) view.findViewById(R.id.curriculum_tv_course5);
-                int w = ((n + 6) % 7 == 0 ? 7 : (n + 6) % 7);
-                tvCourse1.setText(cc.getCourses().get(w + "-1"));
-                tvCourse2.setText(cc.getCourses().get(w + "-2"));
-                tvCourse3.setText(cc.getCourses().get(w + "-3"));
-                tvCourse4.setText(cc.getCourses().get(w + "-4"));
-                tvCourse5.setText(cc.getCourses().get(w + "-5"));
-                //填充布局
-                views.add(view);
-            }
-        }
-
-        @Override  
-        public Object instantiateItem(ViewGroup container, int position) {    
-            View view = views.get(position);
-            container.addView(view);  
-            return view;
-        }
-
-        @Override  
-        public void destroyItem(ViewGroup container, int position, Object object) {  
-            container.removeView(views.get(position));
-        }
-
-        @Override
-        public int getCount() {
-            return weekdayNames.length;
-        }
-
-        @Override
-        public CharSequence getPageTitle(int position) {
-            return weekdayNames[position];
-        }
-
-        @Override
-        public boolean isViewFromObject(View view, Object obj) {
-            return view == obj;
-        }
-
-    }
-
-    private int current = 0;
-
-    @OnPageChange(value = R.id.curriculum_vp_root, callback = OnPageChange.Callback.PAGE_SCROLLED)
-    public void onPageChange(int position, float positionOffset, int positionOffsetPixels) {
-        //判断current
-        if (positionOffset == 0.0f) { //静止
-            current = position;
-        }
-        else if (position >= current) {
-            current = position;
-        }
-        else if (current - position >= 2) {
-            current = position + 1;
-        }
-        //根据current跳转
-        if (current == 1) { //周日
-            vpRoot.setCurrentItem(8, false);
-        }
-        else if (current == 9) { //周一
-            vpRoot.setCurrentItem(2, false);
-        }
     }
 
 }
--- app/src/main/java/com/lntu/online/model/Curriculum.java
+++ app/src/main/java/com/lntu/online/model/Curriculum.java
@@ -5,17 +5,8 @@ import java.util.HashMap;
 public class Curriculum extends Model<Curriculum> {
 
     public static final Curriculum dao = new Curriculum();
-    
-    private HashMap<Integer, String> times;
+
     private HashMap<String, String> courses;
-
-    public HashMap<Integer, String> getTimes() {
-        return times;
-    }
-
-    public void setTimes(HashMap<Integer, String> times) {
-        this.times = times;
-    }
 
     public HashMap<String, String> getCourses() {
         return courses;
