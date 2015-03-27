2729d82b8e83d806e2ca39e8545d8b59f09b230e
TakWolf <takwolf@foxmail.com>
2015-03-27 22:39:00 +0800

    成绩查询默认条件改成全部

--- app/src/main/java/com/lntu/online/activity/CourseScoreActivity.java
+++ app/src/main/java/com/lntu/online/activity/CourseScoreActivity.java
@@ -112,7 +112,6 @@ public class CourseScoreActivity extends ActionBarActivity {
         ArrayAdapter<String> spnYearAdp = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, strsYear);
         spnYearAdp.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spnYear.setAdapter(spnYearAdp);
-        spnYear.setSelection(1);
         spnYear.setOnItemSelectedListener(new OnItemSelectedListener() {
 
             @Override
