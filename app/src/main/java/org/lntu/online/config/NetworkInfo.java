9ff76dbe14b680dea7d9c67209eb7f645a4fcd2d
TakWolf <takwolf@foxmail.com>
2015-04-19 17:21:53 +0800

    mac克隆测试

--- app/src/main/java/org/lntu/online/config/NetworkInfo.java
+++ app/src/main/java/org/lntu/online/config/NetworkInfo.java
@@ -3,6 +3,7 @@ package org.lntu.online.config;
 public class NetworkInfo {
 
     public final static String API_HOST = "http://olv2.lntu.org";
+
     //public final static String API_HOST = "http://lntuonline.codingapp.com";
 
 }
