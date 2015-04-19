ed7d15ed202204b3db483143494bdf7c37e0aacf
TakWolf <takwolf@foxmail.com>
2015-04-19 16:11:51 +0800

    更换线上域名

--- app/src/main/java/org/lntu/online/config/NetworkInfo.java
+++ app/src/main/java/org/lntu/online/config/NetworkInfo.java
@@ -2,6 +2,7 @@ package org.lntu.online.config;
 
 public class NetworkInfo {
 
-    public final static String API_HOST = "http://lntuv2.pupboss.com/api";
+    public final static String API_HOST = "http://olv2.lntu.org";
+    //public final static String API_HOST = "http://lntuonline.codingapp.com";
 
 }
