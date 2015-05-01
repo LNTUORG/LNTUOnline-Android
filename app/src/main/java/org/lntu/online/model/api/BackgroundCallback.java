2f7863684833453dfbf954e5b2b03f1e5adcd210
TakWolf <takwolf@foxmail.com>
2015-05-02 02:53:11 +0800

    未评课错误处理

--- app/src/main/java/org/lntu/online/model/api/BackgroundCallback.java
+++ app/src/main/java/org/lntu/online/model/api/BackgroundCallback.java
@@ -3,16 +3,20 @@ package org.lntu.online.model.api;
 import android.content.Context;
 import android.content.Intent;
 
+import com.afollestad.materialdialogs.MaterialDialog;
 import com.lntu.online.R;
 import org.lntu.online.model.entity.ErrorInfo;
 import org.lntu.online.shared.LoginShared;
 import org.lntu.online.ui.activity.AuthErrorActivity;
+import org.lntu.online.ui.activity.OneKeyEvaActivity;
+import org.lntu.online.util.ShipUtils;
+import org.lntu.online.util.ToastUtils;
 
 import retrofit.Callback;
 import retrofit.RetrofitError;
 import retrofit.client.Response;
 
-public class BackgroundCallback<T> implements Callback<T> {
+public abstract class BackgroundCallback<T> implements Callback<T> {
 
     private Context context;
 
@@ -38,6 +42,26 @@ public class BackgroundCallback<T> implements Callback<T> {
                 break;
             case REMOTE_INVOKE_ERROR:
                 handleFailure(context.getString(R.string.network_remote_invoke_error_tip));
+                break;
+            case NOT_EVALUATE:
+                new MaterialDialog.Builder(context)
+                        .title("提示")
+                        .content("您本学期还没有完成评课，暂时无法查看成绩。")
+                        .contentColorRes(R.color.text_color_primary)
+                        .positiveText("去评课")
+                        .positiveColorRes(R.color.color_primary)
+                        .negativeText("取消")
+                        .negativeColorRes(R.color.text_color_primary)
+                        .callback(new MaterialDialog.ButtonCallback() {
+
+                            @Override
+                            public void onPositive(MaterialDialog dialog) {
+                                context.startActivity(new Intent(context, OneKeyEvaActivity.class));
+                            }
+
+                        })
+                        .show();
+                handleFailure("您本学期还没有完成评课，暂时无法查看成绩");
                 break;
             default:
                 if (errorInfo.getStatusCode() >= 500) {
