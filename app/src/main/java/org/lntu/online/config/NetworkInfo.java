248a6e47ffa06fdde2ae67513c3503a77f839135
TakWolf <takwolf@foxmail.com>
2015-05-28 03:05:08 +0800

    第三方库更新

--- app/src/main/java/org/lntu/online/config/NetworkInfo.java
+++ app/src/main/java/org/lntu/online/config/NetworkInfo.java
@@ -2,7 +2,8 @@ package org.lntu.online.config;
 
 public class NetworkInfo {
 
-    public static final String API_HOST = "http://api.online.lntu.org";
+    //public static final String API_HOST = "http://api.online.lntu.org";
+    public static final String API_HOST = "http://192.168.1.105:8080";
     //public static final String API_HOST = "http://lntuonline.codingapp.com";
 
 }
