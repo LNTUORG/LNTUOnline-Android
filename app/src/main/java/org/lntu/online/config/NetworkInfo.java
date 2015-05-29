c8885ebfa406b76cd0563d3b94ace7481a2abb25
TakWolf <takwolf@foxmail.com>
2015-05-29 12:18:56 +0800

    git仓库迁移

--- app/src/main/java/org/lntu/online/config/NetworkInfo.java
+++ app/src/main/java/org/lntu/online/config/NetworkInfo.java
@@ -3,7 +3,7 @@ package org.lntu.online.config;
 public class NetworkInfo {
 
     //public static final String API_HOST = "http://api.online.lntu.org";
-    public static final String API_HOST = "http://192.168.1.105:8080";
+    public static final String API_HOST = "http://192.168.1.102:8080";
     //public static final String API_HOST = "http://lntuonline.codingapp.com";
 
 }
