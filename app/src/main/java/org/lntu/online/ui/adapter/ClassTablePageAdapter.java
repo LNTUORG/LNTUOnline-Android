fac329d5104636e9d757fedeb2efe578753b6949
TakWolf <takwolf@foxmail.com>
2015-06-08 03:02:16 +0800

    课表布局框架

--- app/src/main/java/org/lntu/online/ui/adapter/ClassTablePageAdapter.java
+++ app/src/main/java/org/lntu/online/ui/adapter/ClassTablePageAdapter.java
@@ -1,18 +1,81 @@
 package org.lntu.online.ui.adapter;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v4.view.PagerAdapter;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+
+import com.lntu.online.R;
+
+import org.joda.time.DateTime;
+import org.joda.time.Period;
+import org.joda.time.PeriodType;
+import org.lntu.online.util.ToastUtils;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ClassTablePageAdapter extends PagerAdapter {
 
+    private Context context;
+    private LayoutInflater inflater;
+    private List<View> viewPool;
+    private int pageCount;
+
+    public ClassTablePageAdapter(Context context) {
+        this.context = context;
+        inflater = LayoutInflater.from(context);
+
+        // 计算page size
+        DateTime nowTime = new DateTime();
+       // if (nowTime) {
+
+       // }
+        DateTime startTime = new DateTime("2015-3-1");
+        DateTime endTime = new DateTime("2015-9-1");
+
+        Period p = new Period(startTime, endTime, PeriodType.days());
+
+        ToastUtils.with(context).show(p.getDays() + "");
+
+    }
+
     @Override
     public int getCount() {
-        return 0;
+        return 20;
     }
 
     @Override
     public boolean isViewFromObject(View view, Object object) {
-        return false;
+        return view == object;
+    }
+
+    @Override
+    public Object instantiateItem(ViewGroup container, int position) {
+        View convertView;
+        if (viewPool == null) {
+            viewPool = new ArrayList<View>();
+            for (int n = 0; n < 3; n++) {
+                View viewItem = inflater.inflate(R.layout.activity_class_table_page_item, container, false);
+
+                viewPool.add(viewItem);
+            }
+        }
+
+
+        convertView = viewPool.get(position % 3);
+
+
+        container.removeView(convertView);
+        container.addView(convertView);
+        return convertView;
+    }
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+        // 忽略
     }
 
 }
--- app/src/main/java/org/lntu/online/ui/fragment/ClassTablePageFragment.java
+++ app/src/main/java/org/lntu/online/ui/fragment/ClassTablePageFragment.java
@@ -1,6 +1,7 @@
 package org.lntu.online.ui.fragment;
 
 import android.os.Bundle;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -8,11 +9,18 @@ import android.view.ViewGroup;
 import com.lntu.online.R;
 
 import org.lntu.online.model.entity.ClassTable;
+import org.lntu.online.ui.adapter.ClassTablePageAdapter;
 import org.lntu.online.ui.base.ClassTableFragment;
 
 import butterknife.ButterKnife;
+import butterknife.InjectView;
 
 public class ClassTablePageFragment extends ClassTableFragment {
+
+    @InjectView(R.id.class_table_page_view_pager)
+    protected ViewPager viewPager;
+
+    private ClassTablePageAdapter adapter;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -23,11 +31,20 @@ public class ClassTablePageFragment extends ClassTableFragment {
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         ButterKnife.inject(this, view);
+
+        adapter = new ClassTablePageAdapter(getActivity());
+        viewPager.setAdapter(adapter);
     }
 
     @Override
     public void updateDataView(ClassTable classTable) {
+        for (ClassTable.Course course : classTable.getCourses()) {
+            for (ClassTable.TimeAndPlace timeAndPlace : course.getTimesAndPlaces()) {
 
+                // timeAndPlace.
+
+            }
+        }
     }
 
 }
