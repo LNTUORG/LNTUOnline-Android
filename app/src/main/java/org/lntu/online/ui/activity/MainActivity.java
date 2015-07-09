0daad9868f0f79588f6daa5dc9cbf040bc147c18
TakWolf <takwolf@foxmail.com>
2015-07-10 02:19:51 +0800

    主导航实现

--- app/src/main/java/org/lntu/online/ui/activity/MainActivity.java
+++ app/src/main/java/org/lntu/online/ui/activity/MainActivity.java
@@ -12,8 +12,10 @@ import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.view.animation.LinearInterpolator;
+import android.widget.CheckedTextView;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.afollestad.materialdialogs.MaterialDialog;
 import com.lntu.online.R;
@@ -29,21 +31,37 @@ import org.lntu.online.util.ShipUtils;
 import org.lntu.online.util.ToastUtils;
 import org.lntu.online.util.UpdateUtils;
 
+import java.util.List;
+
 import butterknife.ButterKnife;
 import butterknife.InjectView;
+import butterknife.InjectViews;
 import butterknife.OnClick;
 import retrofit.client.Response;
 
 public class MainActivity extends BaseActivity {
 
-    public static final String KEY_BACK_TO_ENTRY = "backToEntry";
-
-    @InjectView(R.id.toolbar)
-    protected Toolbar toolbar;
-
+    // 抽屉导航布局
     @InjectView(R.id.main_drawer_layout)
     protected DrawerLayout drawerLayout;
 
+    // 导航项的消息数
+    @InjectView(R.id.main_left_tv_badger_my_class_table)
+    protected TextView tvBadgerMyClassTable;
+
+    @InjectView(R.id.main_left_tv_badger_grades_query)
+    protected TextView tvBadgerGradesQuery;
+
+    @InjectView(R.id.main_left_tv_badger_exam_plan)
+    protected TextView tvBadgerExamPlan;
+
+    @InjectView(R.id.main_left_tv_badger_senate_notice)
+    protected TextView tvBadgerSenateNotice;
+
+    @InjectView(R.id.main_left_tv_badger_notification)
+    protected TextView tvBadgerNotification;
+
+    // 导航顶部控件
     @InjectView(R.id.main_left_img_avatar)
     protected ImageView imgAvatar;
 
@@ -56,8 +74,24 @@ public class MainActivity extends BaseActivity {
     @InjectView(R.id.main_left_tv_class_info)
     protected TextView tvClassInfo;
 
-    @InjectView(R.id.main_left_anim)
-    protected View iconAnim;
+    // 主要导航项
+    @InjectViews({
+            R.id.main_left_btn_my_class_table,
+            R.id.main_left_btn_grades_query,
+            R.id.main_left_btn_exam_plan,
+            R.id.main_left_btn_senate_notice
+    })
+    protected List<CheckedTextView> navItemList;
+
+
+
+
+
+
+    public static final String KEY_BACK_TO_ENTRY = "backToEntry";
+
+    @InjectView(R.id.toolbar)
+    protected Toolbar toolbar;
 
     @InjectView(R.id.main_recycler_view)
     protected RecyclerView recyclerView;
@@ -65,26 +99,25 @@ public class MainActivity extends BaseActivity {
     private boolean asyncStudentFlag = false;
     private long firstBackKeyTime = 0;
 
-    private Animation anim;
-    private boolean animPlaying = false;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.inject(this);
 
+        drawerLayout.setDrawerShadow(R.drawable.navigation_drawer_shadow, GravityCompat.START);
+
+
+
+
+
+
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_menu_white_24dp);
 
-        drawerLayout.setDrawerShadow(R.drawable.navigation_drawer_shadow, GravityCompat.START);
-
         recyclerView.setLayoutManager(new GridLayoutManager(this, 3, GridLayoutManager.VERTICAL, false));
         recyclerView.setAdapter(new MainAdapter(this));
-
-        anim = AnimationUtils.loadAnimation(this, R.anim.data_loading);
-        anim.setInterpolator(new LinearInterpolator());
 
         UpdateUtils.update(this);
 
@@ -152,37 +185,6 @@ public class MainActivity extends BaseActivity {
         });
     }
 
-    @OnClick({
-            R.id.main_action_browser,
-            R.id.main_action_market,
-            R.id.main_action_feedback,
-            R.id.main_action_share,
-            R.id.main_action_settings,
-            R.id.main_action_about
-    })
-    protected void onDrawerItemSelected(View view) {
-        switch (view.getId()) {
-            case R.id.main_action_browser:
-                ShipUtils.webOnline(this);
-                break;
-            case R.id.main_action_market:
-                ShipUtils.appStore(this);
-                break;
-            case R.id.main_action_feedback:
-                startActivity(new Intent(this, AdviceActivity.class));
-                break;
-            case R.id.main_action_share:
-                ShipUtils.share(this);
-                break;
-            case R.id.main_action_settings:
-                startActivity(new Intent(this, SettingActivity.class));
-                break;
-            case R.id.main_action_about:
-                startActivity(new Intent(this, AboutActivity.class));
-                break;
-        }
-    }
-
     @Override
     public void onBackPressed() {
         if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
@@ -198,6 +200,14 @@ public class MainActivity extends BaseActivity {
         }
     }
 
+
+
+
+
+
+    /**
+     * 注销按钮事件
+     */
     @OnClick(R.id.main_left_btn_logout)
     protected void onBtnLogoutClick() {
         new MaterialDialog.Builder(this)
@@ -217,15 +227,196 @@ public class MainActivity extends BaseActivity {
                 .show();
     }
 
-    @OnClick(R.id.main_left_btn_anim)
-    protected void onBtnAnimClick() {
-        if (!animPlaying) {
-            iconAnim.startAnimation(anim);
-            animPlaying = true;
-        } else {
-            iconAnim.clearAnimation();
-            animPlaying = false;
+    /**
+     * 根据id选中主导航中的项，id不匹配则不选中
+     */
+    public void checkNavigationItem(int id) {
+        for (CheckedTextView navItem : navItemList) {
+            navItem.setChecked(navItem.getId() == id);
         }
     }
+
+    /**
+     * 主导航项单击事件
+     */
+    @OnClick({
+            R.id.main_left_btn_my_class_table,
+            R.id.main_left_btn_grades_query,
+            R.id.main_left_btn_exam_plan,
+            R.id.main_left_btn_senate_notice
+    })
+    public void onNavigationItemMainClick(CheckedTextView itemView) {
+        switch (itemView.getId()) {
+            case R.id.main_left_btn_my_class_table:
+                drawerLayout.setDrawerListener(myClassTableDrawerListener);
+                break;
+            case R.id.main_left_btn_grades_query:
+                drawerLayout.setDrawerListener(gradesQueryDrawerListener);
+                break;
+            case R.id.main_left_btn_exam_plan:
+                drawerLayout.setDrawerListener(examPlanDrawerListener);
+                break;
+            case R.id.main_left_btn_senate_notice:
+                drawerLayout.setDrawerListener(senateNoticeDrawerListener);
+                break;
+            default:
+                drawerLayout.setDrawerListener(null);
+                break;
+        }
+        checkNavigationItem(itemView.getId());
+        drawerLayout.closeDrawers();
+    }
+
+    /**
+     * 次要导航项单击事件
+     */
+    @OnClick({
+            R.id.main_left_img_avatar,
+            R.id.main_left_btn_notification,
+            R.id.main_left_btn_setting,
+            R.id.main_left_btn_about
+    })
+    public void onNavigationItemOtherClick(View itemView) {
+        switch (itemView.getId()) {
+            case R.id.main_left_img_avatar:
+                drawerLayout.setDrawerListener(studentInfoDrawerListener);
+                break;
+            case R.id.main_left_btn_notification:
+                drawerLayout.setDrawerListener(notificationDrawerListener);
+                break;
+            case R.id.main_left_btn_setting:
+                drawerLayout.setDrawerListener(settingDrawerListener);
+                break;
+            case R.id.main_left_btn_about:
+                drawerLayout.setDrawerListener(aboutDrawerListener);
+                break;
+            default:
+                drawerLayout.setDrawerListener(null);
+                break;
+        }
+        drawerLayout.closeDrawers();
+    }
+
+    /**
+     * 全部的Drawer事件监听器，这么做的目的是让Drawer关闭之后再触发事件
+     */
+    private DrawerLayout.DrawerListener myClassTableDrawerListener = new DrawerLayout.SimpleDrawerListener() {
+
+        @Override
+        public void onDrawerClosed(View drawerView) {
+            // TODO
+            Toast.makeText(MainActivity.this, "我的课表", Toast.LENGTH_LONG).show();
+
+            drawerLayout.setDrawerListener(null);
+        }
+
+    };
+
+    private DrawerLayout.DrawerListener gradesQueryDrawerListener = new DrawerLayout.SimpleDrawerListener() {
+
+        @Override
+        public void onDrawerClosed(View drawerView) {
+            // TODO
+            Toast.makeText(MainActivity.this, "成绩查询", Toast.LENGTH_LONG).show();
+
+            drawerLayout.setDrawerListener(null);
+        }
+
+    };
+
+    private DrawerLayout.DrawerListener examPlanDrawerListener = new DrawerLayout.SimpleDrawerListener() {
+
+        @Override
+        public void onDrawerClosed(View drawerView) {
+            // TODO
+            Toast.makeText(MainActivity.this, "考试安排", Toast.LENGTH_LONG).show();
+
+            drawerLayout.setDrawerListener(null);
+        }
+
+    };
+
+    private DrawerLayout.DrawerListener senateNoticeDrawerListener = new DrawerLayout.SimpleDrawerListener() {
+
+        @Override
+        public void onDrawerClosed(View drawerView) {
+            // TODO
+            Toast.makeText(MainActivity.this, "教务公告", Toast.LENGTH_LONG).show();
+
+            drawerLayout.setDrawerListener(null);
+        }
+
+    };
+
+    private DrawerLayout.DrawerListener studentInfoDrawerListener = new DrawerLayout.SimpleDrawerListener() {
+
+        @Override
+        public void onDrawerClosed(View drawerView) {
+            startActivity(new Intent(MainActivity.this, StudentInfoActivity.class));
+            drawerLayout.setDrawerListener(null);
+        }
+
+    };
+
+    private DrawerLayout.DrawerListener notificationDrawerListener = new DrawerLayout.SimpleDrawerListener() {
+
+        @Override
+        public void onDrawerClosed(View drawerView) {
+            startActivity(new Intent(MainActivity.this, NotificationActivity.class));
+            drawerLayout.setDrawerListener(null);
+        }
+
+    };
+
+    private DrawerLayout.DrawerListener settingDrawerListener = new DrawerLayout.SimpleDrawerListener() {
+
+        @Override
+        public void onDrawerClosed(View drawerView) {
+            startActivity(new Intent(MainActivity.this, SettingActivity.class));
+            drawerLayout.setDrawerListener(null);
+        }
+
+    };
+
+    private DrawerLayout.DrawerListener aboutDrawerListener = new DrawerLayout.SimpleDrawerListener() {
+
+        @Override
+        public void onDrawerClosed(View drawerView) {
+            startActivity(new Intent(MainActivity.this, AboutActivity.class));
+            drawerLayout.setDrawerListener(null);
+        }
+
+    };
+
+
+
+
+
+
+
+
+
+
+
+    /*
+    protected void onDrawerItemSelected(View view) {
+        switch (view.getId()) {
+            case R.id.main_action_browser:
+                ShipUtils.webOnline(this);
+                break;
+            case R.id.main_action_market:
+                ShipUtils.appStore(this);
+                break;
+            case R.id.main_action_feedback:
+                startActivity(new Intent(this, AdviceActivity.class));
+                break;
+            case R.id.main_action_share:
+                ShipUtils.share(this);
+                break;
+
+        }
+    }
+    */
+
 
 }
