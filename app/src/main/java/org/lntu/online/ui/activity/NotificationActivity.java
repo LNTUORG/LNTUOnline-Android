6fe576d28ac63bf7b61f30e7a4a2a5134edb90b7
TakWolf <takwolf@foxmail.com>
2015-07-10 13:30:54 +0800

    系统消息页面

--- app/src/main/java/org/lntu/online/ui/activity/NotificationActivity.java
+++ app/src/main/java/org/lntu/online/ui/activity/NotificationActivity.java
@@ -1,7 +1,41 @@
 package org.lntu.online.ui.activity;
 
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+
+import com.lntu.online.R;
+
 import org.lntu.online.ui.base.BaseActivity;
+import org.lntu.online.util.AppUtils;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
 
 public class NotificationActivity extends BaseActivity {
+
+    @InjectView(R.id.notification_toolbar)
+    protected Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notification);
+        ButterKnife.inject(this);
+
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
 
 }
