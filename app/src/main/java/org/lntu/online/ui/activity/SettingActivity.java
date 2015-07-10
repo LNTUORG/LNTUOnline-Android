f3739b3cd0137b85591e05d87a517d6bda0e6fa4
TakWolf <takwolf@foxmail.com>
2015-07-10 11:29:09 +0800

    设置页面界面

--- app/src/main/java/org/lntu/online/ui/activity/SettingActivity.java
+++ app/src/main/java/org/lntu/online/ui/activity/SettingActivity.java
@@ -1,10 +1,40 @@
 package org.lntu.online.ui.activity;
 
 import android.os.Bundle;
-import android.support.annotation.Nullable;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+
+import com.lntu.online.R;
 
 import org.lntu.online.ui.base.BaseActivity;
 
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+
 public class SettingActivity extends BaseActivity {
+
+    @InjectView(R.id.setting_toolbar)
+    protected Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setting);
+        ButterKnife.inject(this);
+
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
 
 }
