29cb983da5d1d1410c91d19adbf6d6b3db4747b6
TakWolf <takwolf@foxmail.com>
2015-07-13 02:11:57 +0800

    代码整理

--- app/src/main/java/org/lntu/online/ui/activity/MainActivity.java
+++ app/src/main/java/org/lntu/online/ui/activity/MainActivity.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.CheckedTextView;
 import android.widget.ImageView;
