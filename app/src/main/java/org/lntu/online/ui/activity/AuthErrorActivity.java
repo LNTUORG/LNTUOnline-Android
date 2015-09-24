f2673b5b37103f49e6a6d79b186c56f3d2a4b8f6
TakWolf <takwolf@foxmail.com>
2015-09-24 21:35:59 +0800

    代码迁移

--- app/src/main/java/org/lntu/online/ui/activity/AuthErrorActivity.java
+++ app/src/main/java/org/lntu/online/ui/activity/AuthErrorActivity.java
@@ -1,36 +1,41 @@
 package org.lntu.online.ui.activity;
 
+import android.app.Activity;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 
+import com.afollestad.materialdialogs.MaterialDialog;
+
 import org.lntu.online.R;
-import org.lntu.online.ui.base.BaseActivity;
 
-import butterknife.ButterKnife;
-import butterknife.OnClick;
-
-public class AuthErrorActivity extends BaseActivity {
+public class AuthErrorActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_auth_error);
-        ButterKnife.bind(this);
-    }
 
-    @OnClick(R.id.auth_error_btn_close)
-    protected void onBtnCloseClick() {
-        finish();
-    }
+        new MaterialDialog.Builder(this)
+                .backgroundColorRes(android.R.color.white)
+                .title(R.string.auth_error)
+                .titleColorRes(R.color.text_color_primary)
+                .content(R.string.auth_error_tip)
+                .contentColorRes(R.color.text_color_secondary)
+                .positiveText(R.string.ok)
+                .positiveColorRes(R.color.color_accent)
+                .dismissListener(new DialogInterface.OnDismissListener() {
 
-    @Override
-    protected void onDestroy() {
-        Intent intent = new Intent(this, MainActivity.class);
-        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-        intent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
-        intent.putExtra(MainActivity.KEY_BACK_TO_ENTRY, true);
-        startActivity(intent);
-        super.onDestroy();
+                    @Override
+                    public void onDismiss(DialogInterface dialog) {
+                        Intent intent = new Intent(AuthErrorActivity.this, MainActivity.class);
+                        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                        intent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
+                        intent.putExtra(MainActivity.KEY_BACK_TO_ENTRY, true);
+                        startActivity(intent);
+                    }
+
+                })
+                .show();
     }
 
 }
--- app/src/main/java/org/lntu/online/ui/activity/NotificationActivity.java
+++
@@ -1,7 +0,0 @@
-package org.lntu.online.ui.activity;
-
-import org.lntu.online.ui.base.BaseActivity;
-
-public class NotificationActivity extends BaseActivity {
-
-}
