65b074765852ae72da1edf4cf1bbd2c58690df70
TakWolf <takwolf@foxmail.com>
2015-09-25 01:00:43 +0800

    教务公告

---
+++ app/src/main/java/org/lntu/online/ui/activity/SenateNoticeActivity.java
@@ -0,0 +1,68 @@
+package org.lntu.online.ui.activity;
+
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import org.lntu.online.R;
+import org.lntu.online.ui.listener.NavigationFinishClickListener;
+import org.lntu.online.util.ShipUtils;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class SenateNoticeActivity extends BaseActivity implements Toolbar.OnMenuItemClickListener {
+
+    private static final String URL = "http://60.18.131.133:8090/lntu/pub_message/messagesplitepageopenwindow.jsp?fmodulecode=5100&modulecode=5100&messagefid=5100";
+
+    @Bind(R.id.senate_notice_toolbar)
+    protected Toolbar toolbar;
+
+    @Bind(R.id.senate_notice_web_view)
+    protected WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_senate_notice);
+        ButterKnife.bind(this);
+
+        toolbar.setNavigationOnClickListener(new NavigationFinishClickListener(this));
+        toolbar.inflateMenu(R.menu.senate_notice);
+        toolbar.setOnMenuItemClickListener(this);
+
+        webView.loadUrl(URL);
+        webView.setWebViewClient(new WebViewClient() {
+
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return true;
+            }
+
+        });
+    }
+
+    @Override
+    public boolean onMenuItemClick(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_open_in_browser:
+                ShipUtils.openInBrowser(this, URL);
+                return true;
+            default:
+                return false;
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+}
