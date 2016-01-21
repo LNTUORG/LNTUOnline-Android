85d44f4d32776e359e5ba008ac02c679b2494d1d
TakWolf <takwolf@foxmail.com>
2016-01-22 00:51:12 +0800

    LoginToken重构

--- app/src/main/java/org/lntu/online/storage/LoginShared.java
+++ app/src/main/java/org/lntu/online/storage/LoginShared.java
@@ -1,6 +1,7 @@
 package org.lntu.online.storage;
 
 import android.content.Context;
+import android.text.TextUtils;
 
 import org.joda.time.DateTime;
 import org.lntu.online.model.entity.ClassTable;
@@ -14,51 +15,62 @@ public final class LoginShared {
 
     private LoginShared() {}
 
-    private static final String TAG = LoginShared.class.getSimpleName();
-
-    //============
-    // 用户登录状态
-    //============
+    private static final String TAG = "LoginShared";
 
     private static final String KEY_USER_ID = "userId";
-    private static final String KEY_USER_TYPE = "userType";
     private static final String KEY_LOGIN_TOKEN = "loginToken";
-    private static final String KEY_EXPIRES_AT = "expiresAt";
     private static final String KEY_HOLD_ONLINE = "holdOnline";
 
-    public static void login(Context context, LoginInfo info, boolean isHoldOnline) {
+    private static String userId;
+    private static String loginToken;
+    private static Boolean holdOnline;
+
+    public static void login(Context context, LoginInfo info, boolean holdOnline) {
         SharedWrapper sharedWrapper = SharedWrapper.with(context, TAG);
         sharedWrapper.setString(KEY_USER_ID, info.getUserId());
-        sharedWrapper.setString(KEY_USER_TYPE, info.getUserType().name());
         sharedWrapper.setString(KEY_LOGIN_TOKEN, info.getLoginToken());
-        sharedWrapper.setString(KEY_EXPIRES_AT, new DateTime(info.getExpiresAt()).toString());
-        sharedWrapper.setBoolean(KEY_HOLD_ONLINE, isHoldOnline); // 是否保持在线状态
+        sharedWrapper.setBoolean(KEY_HOLD_ONLINE, holdOnline); // 是否保持在线状态
+        userId = info.getUserId();
+        loginToken = info.getLoginToken();
+        LoginShared.holdOnline = holdOnline;
     }
 
     public static void logout(Context context) {
         SharedWrapper.with(context, TAG).clear();
+        userId = null;
+        loginToken = null;
+        holdOnline = null;
     }
 
     public static String getUserId(Context context) {
-        return SharedWrapper.with(context, TAG).getString(KEY_USER_ID, null);
-    }
-
-    public static UserType getUserType(Context context) {
-        return UserType.valueOf(SharedWrapper.with(context, TAG).getString(KEY_USER_TYPE, UserType.STUDENT.name()));
+        if (TextUtils.isEmpty(userId)) {
+            userId = SharedWrapper.with(context, TAG).getString(KEY_USER_ID, null);
+        }
+        return userId;
     }
 
     public static String getLoginToken(Context context) {
-        return SharedWrapper.with(context, TAG).getString(KEY_LOGIN_TOKEN, null);
-    }
-
-    public static Date getExpiresAt(Context context) {
-        return new DateTime(SharedWrapper.with(context, TAG).getString(KEY_EXPIRES_AT, "1900-1-1")).toDate();
+        if (TextUtils.isEmpty(loginToken)) {
+            loginToken = SharedWrapper.with(context, TAG).getString(KEY_LOGIN_TOKEN, null);
+        }
+        return loginToken;
     }
 
     public static boolean isHoldOnline(Context context) {
-        return SharedWrapper.with(context, TAG).getBoolean(KEY_HOLD_ONLINE, false);
+        if (holdOnline == null) {
+            holdOnline = SharedWrapper.with(context, TAG).getBoolean(KEY_HOLD_ONLINE, false);
+        }
+        return holdOnline;
     }
 
+
+
+
+
+
+
+
+    
     //=========
     // 缓存数据
     //=========
